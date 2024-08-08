99be4046b7278bbc9f862a40ddd0184e03fc863a
Susan <susansewe@gmail.com>
2024-08-08 07:52:54 +0000

    MinOPerations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Performs minimum Operations """
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    """
+    if n <= 0:
+        return 0
+
+    operations = 0
+    current = 1 """the initial number of 'H's"""
+    while current < n: """find the smallest factor of n """
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                operations += i
+                current *= i
+                break
+        else:
+            operations += (n - current)
+            current = n
+
+    return operations
